d89ae8c569fbba0b91e1515fe9b4d257b7e00bf4
pwdd <ainapinto@gmail.com>
2016-03-03 12:25:47 -0800

    paginate 01

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -84,4 +84,20 @@ class Racer
   def updated_at
     nil
   end
+
+  def self.paginate(params)
+    page = (params[:page] || 1).to_i
+    limit = (params[:per_page] || 30).to_i
+    skip = (page - 1) * limit
+
+    racers = all({}, {number: 1}, skip, limit).inject([]) do |memo, doc|
+      memo << Racer.new(doc)
+    end
+
+    total = all.count
+
+    WillPaginate::Collection.create(page, limit, total) do |pager|
+      pager.replace(racers)
+    end
+  end
 end
